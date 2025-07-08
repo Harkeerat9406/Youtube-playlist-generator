269300c7f5886eec949243e0a5b1666a7f4c2e66
Harkeerat9406 <pharkeeratsingh@gmail.com>
2025-07-08 11:35:39 +0530

    Fixed client secret loading error, made separate secret variables and now importing only secret variables instead of whole client secret json.

--- app.py
+++ app.py
@@ -14,8 +14,18 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv('flask_secret_key')
 
-client_secret_json_str = os.getenv("google_client_secret_json")
-client_config = json.loads(client_secret_json_str)
+client_config = {
+    "web": {
+        "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+        "project_id": os.getenv("GOOGLE_PROJECT_ID"),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret": os.getenv("GOOGLE_CLIENT_SECRET"),
+        "redirect_uris": ["https://morphify-delta.vercel.app/oauth2callback"],
+        "javascript_origins": ["https://morphify-delta.vercel.app"]
+    }
+}
 
 
 genai.configure(api_key = os.getenv("gemini_api"))
